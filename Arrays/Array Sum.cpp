7adfc89a57161a3993122c6ed87857de4ca3ce73
Surya N <cool.surya1906@gmail.com>
2021-05-28 18:24:05 +0530

    Create Array Sum.cpp

---
+++ Arrays/Array Sum.cpp
@@ -0,0 +1,43 @@
+/* 
+Array Sum
+
+Given an array of length N, you need to find and print the sum of all elements of the array.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Sum
+Constraints :
+1 <= N <= 10^6
+Sample Input :
+3
+9 8 9
+Sample Output :
+26
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    /* Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    
+    int arr[1000000];
+    int n,sum = 0;
+    cin >> n;
+    
+    for (int i = 0;i < n;i++)
+    {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0;i < n;i++)
+    {
+        sum = sum + arr[i];
+    }
+    
+    cout << sum << "\n";
+    
+}
